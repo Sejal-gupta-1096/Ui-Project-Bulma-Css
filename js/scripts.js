6cc9d68ac0fb86c388e47dc3af1cba7aa3c8c875
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-09-23 14:06:27 +0530

    Mobile view nav

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
-
+    $(".navbar-item").toggleClass("navbar-item-is-active");
 });
 
 });
